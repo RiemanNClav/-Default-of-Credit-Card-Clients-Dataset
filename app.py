065dc7f4f5c230d318fee93f4adeefd559e94992
AngelClavellina <angel.chavez.clavellina@gmail.com>
2024-12-18 16:32:34 -0600

    cambios finales 1

--- app.py
+++ app.py
@@ -1,52 +1,54 @@
 from flask import Flask, request, render_template
 import numpy as np
 import pandas as pd
+import sys
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.dirname(os.path.dirname(current_dir))
+sys.path.insert(0, project_root)
+
 
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
 application = Flask(__name__)
 app = application
 
-# Cargar el conjunto de datos (ajusta el nombre del archivo o la ruta según tu caso)
 df = pd.read_csv('artifacts/train.csv')
 
-# Obtener valores únicos de las categorías
-companies = df['company'].unique()
-toppings = df['topping'].unique()
-variants = df['variant'].unique()
-sizes = df['size'].unique()
-extra_sauces = df['extra_sauce'].unique()
-extra_cheeses = df['extra_cheese'].unique()
-extra_mushrooms = df['extra_mushrooms'].unique()
+SEX = df['SEX'].unique()
+EDUCATION = ["0", "1", "2", "3", "4"]
+MARRIAGE = df['MARRIAGE'].unique()
+AGE = df['AGE'].unique()
 
-# Ruta para la página de inicio
+
 @app.route('/')
 def index():
     return render_template('index.html') 
 
-# Ruta para predecir datos
 @app.route('/predictdata', methods=['GET', 'POST'])
 def predict_datapoint():
     if request.method == 'GET':
         return render_template('home.html',
-                               companies=companies,
-                               toppings=toppings,
-                               variants=variants,
-                               sizes=sizes,
-                               extra_sauces=extra_sauces,
-                               extra_cheeses=extra_cheeses,
-                               extra_mushrooms=extra_mushrooms)
+                               SEXs=SEX,
+                               EDUCATIONs=EDUCATION,
+                               MARRIAGsE=MARRIAGE,
+                               AGEs=AGE)
     else:
         data = CustomData(
-            diameter=float(request.form.get('diameter')),
-            company=request.form.get('company'),
-            topping=request.form.get('topping'),
-            variant=request.form.get('variant'),
-            size=request.form.get('size'),
-            extra_sauce=request.form.get('extra_sauce'),
-            extra_cheese=request.form.get('extra_cheese'),
-            extra_mushrooms=request.form.get('extra_mushrooms')
+            limit_bal=int(request.form.get('LIMIT_BAL')),
+            sex=request.form.get('SEX'),
+            education=request.form.get('EDUCATION'),
+            marriage=request.form.get('MARRIAGE'),
+            age=request.form.get('AGE'),
+            pay_1=float(request.form.get('PAY_1')),
+            pay_2=float(request.form.get('PAY_2')),
+            pay_3=float(request.form.get('PAY_3')),
+            pay_4=float(request.form.get('PAY_4')),
+            pay_5=float(request.form.get('PAY_5')),
+            pay_6=float(request.form.get('PAY_6'))
         )
+
         pred_df = data.get_data_as_data_frame()
         print(pred_df)
         print("Before Prediction")
@@ -55,15 +57,12 @@ def predict_datapoint():
         print("Mid Prediction")
         results = predict_pipeline.predict(pred_df)
         print("After Prediction")
-        return render_template('home.html', 
-                               results=results[0],
-                               companies=companies,
-                               toppings=toppings,
-                               variants=variants,
-                               sizes=sizes,
-                               extra_sauces=extra_sauces,
-                               extra_cheeses=extra_cheeses,
-                               extra_mushrooms=extra_mushrooms)
+        return render_template('home.html',
+                               result=results[0],
+                               SEXs=SEX,
+                               EDUCATIONs=EDUCATION,
+                               MARRIAGsE=MARRIAGE,
+                               AGEs=AGE)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
